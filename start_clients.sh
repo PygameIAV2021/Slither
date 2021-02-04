18883ca70b77ed1a00c27908f358a0ba7436523b
DustinOnGithub <dp.heise@gmail.com>
2021-02-04 13:40:40 +0100

    arguments for debug script

--- start_clients.sh
+++ start_clients.sh
@@ -13,7 +13,7 @@ THIS IS A DEBUG SCRIPT!
 
 for i in {1..10}
 do
-  echo "start client $i"
-  python3 start_multiplayer.py &
+  echo "start client $i and connect to server $1:$2"
+  python3 start_multiplayer.py $1 $2 &> /dev/null &
   sleep 0.5
 done
